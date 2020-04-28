4c7aafaeb9fe2a4c83625bbf97585e78eb441beb
itsrealoj <stephenojwang1040@gmail.com>
2020-04-28 15:57:19 +0300

    pulled data correctly in the component did mount

--- src/component/RandomQuote.js
+++ src/component/RandomQuote.js
@@ -7,6 +7,19 @@ class RandomQuote extends Component {
       quote: 'be water!',
     };
   }
+
+  componentDidMount() {
+    fetch(
+      'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
+    )
+      .then((results) => {
+        return results;
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }
+
   render() {
     return (
       <div>
