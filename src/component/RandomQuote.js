b9170ac4b32e83274cd8b2c15dbc3a5218d2e0d4
itsrealoj <stephenojwang1040@gmail.com>
2020-04-28 19:47:16 +0300

    successfully picking rando quote

--- src/component/RandomQuote.js
+++ src/component/RandomQuote.js
@@ -5,15 +5,31 @@ class RandomQuote extends Component {
     super(props);
     this.state = {
       quote: 'be water!',
+      data: [],
     };
   }
 
   componentDidMount() {
+    let data = [];
+    let quote = '';
+    let author = '';
+
     fetch(
       'https://gist.githubusercontent.com/shreyasminocha/7d5dedafc1fe158f82563c1223855177/raw/325d51aca7165b2498971afcff9bed286a52dc0e/quotes.json',
     )
       .then((data) => data.json())
-      .then((d) => console.log(d));
+      .then((d) => {
+        data.push(d[Math.floor(Math.random() * Math.ceil(100))]);
+        // console.log(data[0]);
+        quote = data[0].quote;
+        author = data[0].author;
+        console.log(`quote: ${quote}`);
+        console.log(`author: ${author}`);
+        this.setState({
+          quote: quote,
+          //   data: [...data],
+        });
+      });
   }
 
   render() {
