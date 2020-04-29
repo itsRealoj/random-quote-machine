c2153b7f12f6b5b60ff809f6b5aa1ee16add8dd7
itsrealoj <stephenojwang1040@gmail.com>
2020-04-29 10:54:57 +0300

    Updates

---
+++ precache-manifest.53d4d52c2c9012fab0b79d02a6694387.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72db9a19ff4d9095a8a55d581d14a68d",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "0a03855299c7350451a1",
+    "url": "/random-quote-machine/static/js/2.6522be4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.6522be4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9aa814b2f8561736148",
+    "url": "/random-quote-machine/static/js/main.9d4bec7d.chunk.js"
+  },
+  {
+    "revision": "21c559728549918444b9",
+    "url": "/random-quote-machine/static/js/runtime-main.d08601ed.js"
+  }
+]);
